bfe7beb971775a16bbdfe36793c3bd73321f1baf
TurtleOld <shurpo.alexander@gmail.com>
2021-05-30 23:11:14 +0300

    добавил выборку по неделям

--- app.py
+++ app.py
@@ -8,22 +8,24 @@ app = Flask(__name__)
 def get_name_seller():
     cursor.execute("SELECT name_seller FROM receipt GROUP BY name_seller")
     name_sellers = cursor.fetchall()
-    return render_template("index.html", name=name_sellers)
+    return render_template("index.html", name_seller=name_sellers)
 
 
 @app.route('/', methods=["GET", "POST"])
 def get_info():
+    # Выводим список чеков
     name_seller = request.form.get("seller")
-    year = request.form.get("years")
-    if name_seller is not None:
-        cursor.execute(
-            "SELECT date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE "
-            "name_seller=%s ORDER BY date_receipt",
-            (name_seller,))
-        reseller = cursor.fetchall()
-        for item in reseller:
-            total = item[6]
-            return render_template("index.html", reseller=reseller, total=total)
+    cursor.execute(
+        "SELECT date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE name_seller=%s GROUP BY date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
+        (name_seller,))
+    reseller = cursor.fetchall()
+    # На основании числа недели выводим за указанную неделю чеки
+    number_week = request.form.get("weeks")
+    cursor.execute(
+        "SELECT name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE (extract (week from date_receipt)=%s) GROUP BY name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
+        (number_week,))
+    weeks = cursor.fetchall()
+    return render_template("index.html", reseller=reseller, week=weeks)
 
 
 if __name__ == '__main__':
