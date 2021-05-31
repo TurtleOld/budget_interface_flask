2f8cb32a48e4bba8af924f1bd028fae2c0b8d8b2
TurtleOld <shurpo.alexander@gmail.com>
2021-05-31 22:08:26 +0300

    написал одно условие для имени продавца, подготовил почву для будущих изменений и условий

--- app.py
+++ app.py
@@ -15,17 +15,23 @@ def get_name_seller():
 def get_info():
     # Выводим список чеков
     name_seller = request.form.get("seller")
-    cursor.execute(
-        "SELECT date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE name_seller=%s GROUP BY date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
-        (name_seller,))
-    reseller = cursor.fetchall()
+    reseller = ""
+    error_name_seller = ""
+    if name_seller:
+        cursor.execute(
+            "SELECT date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE name_seller=%s GROUP BY date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
+            (name_seller,))
+        reseller = cursor.fetchall()
+    else:
+        error_name_seller = "Необходимо выбрать названия продавца!"
     # На основании числа недели выводим за указанную неделю чеки
     number_week = request.form.get("weeks")
     cursor.execute(
         "SELECT name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE (extract (week from date_receipt)=%s) GROUP BY name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
         (number_week,))
     weeks = cursor.fetchall()
-    return render_template("index.html", reseller=reseller, week=weeks)
+    # Выборка чеков по дате
+    return render_template("index.html", reseller=reseller, week=weeks, error_name=error_name_seller)
 
 
 if __name__ == '__main__':
