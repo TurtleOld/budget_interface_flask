34c662406308dc44d87879cc1ce408d83edcab26
TurtleOld <shurpo.alexander@gmail.com>
2021-06-01 22:28:27 +0300

    Изменил условия if

--- app.py
+++ app.py
@@ -13,25 +13,28 @@ def get_name_seller():
 
 @app.route('/', methods=["GET", "POST"])
 def get_info():
-    # Выводим список чеков
+    number_week = request.form.get("weeks")
+    print(number_week)
+    weeks = ""
+    error_number_week = ""
     name_seller = request.form.get("seller")
+    print(name_seller)
     reseller = ""
     error_name_seller = ""
-    if name_seller:
+    if name_seller == "" and number_week != "":
+        cursor.execute(
+            "SELECT name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE (extract (week from date_receipt)=%s) GROUP BY name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
+            (number_week,))
+        weeks = cursor.fetchall()
+    # Выводим список чеков
+    elif name_seller != "":
         cursor.execute(
             "SELECT date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE name_seller=%s GROUP BY date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
             (name_seller,))
         reseller = cursor.fetchall()
-    else:
-        error_name_seller = "Необходимо выбрать названия продавца!"
-    # На основании числа недели выводим за указанную неделю чеки
-    number_week = request.form.get("weeks")
-    cursor.execute(
-        "SELECT name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE (extract (week from date_receipt)=%s) GROUP BY name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
-        (number_week,))
-    weeks = cursor.fetchall()
-    # Выборка чеков по дате
-    return render_template("index.html", reseller=reseller, week=weeks, error_name=error_name_seller)
+
+    return render_template("index.html", reseller=reseller, week=weeks, error_name=error_name_seller,
+                           error_week=error_number_week)
 
 
 if __name__ == '__main__':
