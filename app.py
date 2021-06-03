635fbdc8d811f024612e77fc375c8f38d2ffc7d4
TurtleOld <shurpo.alexander@gmail.com>
2021-06-03 06:30:23 +0300

    добавил переменные, которые получают день, месяц, год и в функции добавил условия

--- app.py
+++ app.py
@@ -4,23 +4,27 @@ from settings_database import cursor
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def get_name_seller():
     cursor.execute("SELECT name_seller FROM receipt GROUP BY name_seller")
     name_sellers = cursor.fetchall()
     return render_template("index.html", name_seller=name_sellers)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def get_info():
+    days = request.form.get("days")
+    months = request.form.get("months")
+    years = request.form.get("years")
+    get_date = f"{years}-{months}-{days}"
+    print(get_date)
     number_week = request.form.get("weeks")
     print(number_week)
     weeks = ""
-    error_number_week = ""
+    error = ""
     name_seller = request.form.get("seller")
     print(name_seller)
     reseller = ""
-    error_name_seller = ""
     if name_seller == "" and number_week != "":
         cursor.execute(
             "SELECT name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE (extract (week from date_receipt)=%s) GROUP BY name_seller, date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
@@ -32,9 +36,15 @@ def get_info():
             "SELECT date_receipt, time_receipt, name_product, price, quantity, amount, total_sum FROM receipt WHERE name_seller=%s GROUP BY date_receipt, time_receipt, name_product, price, quantity, amount, total_sum ORDER BY date_receipt",
             (name_seller,))
         reseller = cursor.fetchall()
-
-    return render_template("index.html", reseller=reseller, week=weeks, error_name=error_name_seller,
-                           error_week=error_number_week)
+    elif days and months and years:
+        error = "Заполните требуемые поля"
+    elif days == "" and months and years:
+        error = "Не заполнено поле День"
+    elif days and months == "" and years:
+        error = "Не заполнено поле Месяц"
+    elif days and months and years == "":
+        error = "Не заполнено поле Год"
+    return render_template("index.html", reseller=reseller, week=weeks, error=error)
 
 
 if __name__ == '__main__':
