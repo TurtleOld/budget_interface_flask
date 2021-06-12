0ec05f14e0e51b9a4741e4a3a73c757eb57109ad
TurtleOld <shurpo.alexander@gmail.com>
2021-06-12 16:28:23 +0300

    Готовимся к построению графиков

--- charts.py
+++ charts.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template
+import matplotlib.pyplot as plt
+import numpy as np
 
 charts_route = Blueprint("charts", __name__)
 
 
 @charts_route.route("/charts")
 def charts():
-    test = "Проверка"
-    return render_template("charts.html", test=test)
+
+    values = [55, 102, 100]
+    plt.hist(values, bins=2)
+    return render_template("charts.html", test=plt.show())
 
