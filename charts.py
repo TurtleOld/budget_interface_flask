1f09fa2edcbc2a57d1b78e00b114504f6bd57687
TurtleOld <shurpo.alexander@gmail.com>
2021-06-12 22:10:41 +0300

    Тестирование графика

--- charts.py
+++ charts.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 import matplotlib.pyplot as plt
 import numpy as np
+from settings_database import cursor
 
 charts_route = Blueprint("charts", __name__)
 
@@ -8,8 +9,15 @@ charts_route = Blueprint("charts", __name__)
 @charts_route.route("/charts")
 def charts():
     fig1, ax1 = plt.subplots()
-    names = ["One", "Two", "Three"]
-    values = [55, 102, 100]
+    cursor.execute("SELECT name_product, amount FROM receipt GROUP BY name_product, amount")
+    product_amount = cursor.fetchall()
+    product = []
+    amount = []
+    for item in product_amount:
+        product.append(item[0])
+        amount.append(item[1])
+    names = product
+    values = amount
     ax1.pie(values, labels=names)
     plt.show()
     return render_template("charts.html")
