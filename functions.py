9ea66a218f27f720c4150a374c1d74cca19fb182
TurtleOld <shurpo.alexander@gmail.com>
2021-07-14 22:36:59 +0300

    Создал вспомогательную функцию

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def get_full_amount_product(fetchall_item):
     list_amount = []
     for item in fetchall_item:
@@ -14,3 +17,9 @@ def get_total_amount(amount):
         list_amount.append(conversion_to_float_amount)
     summation = sum(list_amount)
     return summation
+
+
+def get_number_month(date_time):
+    date_without_dash = date_time.replace("-", "")
+    return datetime.datetime.strptime(date_without_dash, "%Y%m%d").date().month
+
