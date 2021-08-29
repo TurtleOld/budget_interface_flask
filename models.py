da8f960f2ef12455ae1802086cbd3f9bae4ba9a5
TurtleOld <shurpo.alexander@gmail.com>
2021-08-29 19:29:52 +0300

    Revert "Убрал всё лишнее"
    This reverts commit 98a99cfc2543cb7e66ec5669493fd32edd03c314.

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login = LoginManager()
 
 
 class UserModel(UserMixin, db.Model):
@@ -18,3 +20,8 @@ class UserModel(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return UserModel.query.get(int(id))
